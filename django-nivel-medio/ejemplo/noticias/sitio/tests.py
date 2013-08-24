da50543dfbd4d5b922a306f0718e7a41d05d772a
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-08-24 01:46:19 -0300

    Not finished test example

--- django-nivel-medio/ejemplo/noticias/sitio/tests.py
+++ django-nivel-medio/ejemplo/noticias/sitio/tests.py
@@ -1,16 +1,21 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# coding: utf-8
 from django.test import TestCase
+
+from sitio.models import Noticia
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
+    def test_la_matematica_no_se_rompio(self):
         self.assertEqual(1 + 1, 2)
+
+
+class AlsoSimpleTest(TestCase):
+    def test_inicio_tiene_todas_las_noticias(self):
+        n1 = NoticiaFactory()
+        n2 = NoticiaFactory()
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.code, 200)
+        self.assertIn(n1.titulo, response.content)
+        self.assertIn(n2.titulo, response.content)
